2d7359132a48c54a8562bb2efeaed0739c53c46d
Johan DCS <johandenisson@gmail.com>
2023-12-18 14:29:26 -0500

    Api desplegada

--- src/app.config.ts
+++ src/app.config.ts
@@ -11,12 +11,8 @@ import { TurnoDB } from "./models/turno";
 import { HorarioDB } from "./models/horarios";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: 3306 ||process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    type: "postgres",
+    url: process.env.DB_URL,
     synchronize: true,
     logging: false,
     entities: [personaDB, UsuarioDB, CargoDB, TardanzaDB, AsistenciaDB, FaltasDB, ControlAsistenciaGeneralDB, EmpresaDb, TurnoDB, HorarioDB],
